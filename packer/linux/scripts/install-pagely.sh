2e7a96c27ac76f130a9f3efe830486958a26f2a8
Joshua Eichorn <joshua.eichorn@pagely.com>
2020-05-26 19:54:55 +0000

    move disk init to a script

--- packer/linux/scripts/install-pagely.sh
+++ packer/linux/scripts/install-pagely.sh
@@ -10,3 +10,9 @@ sudo curl -Lsf -o /usr/local/bin/swap.sh \
   "https://raw.githubusercontent.com/chetankapoor/swap/master/swap.sh"
 
 sudo chmod +x /usr/local/bin/swap.sh
+
+echo "Adding storage init script"
+sudo curl -Lsf -o /usr/local/bin/init-storage.sh \
+    "https://gist.githubusercontent.com/jeichorn/18eeb0e088f8eb0a16db0191b0e2d2e0/raw/8401e6c3ae805860b6a4283a76a97d0b40ee430a/init-storage.sh"
+
+sudo chmod +x /usr/local/bin/init-storage.sh
