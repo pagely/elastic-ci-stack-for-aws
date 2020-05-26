f9ac004054cd696b65156f4780d3a1451c4b1f1d
Joshua Eichorn <joshua.eichorn@pagely.com>
2020-05-26 16:19:33 +0000

    update to php 7.4 and sync with upstream

---
+++ packer/linux/scripts/install-php.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo amazon-linux-extras install php7.4
+sudo yum install -y php-xml php-posix php-curl php-composer
--- packer/scripts/install-php.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo amazon-linux-extras install php7.2
-sudo yum install -y php-xml php-posix php-curl
